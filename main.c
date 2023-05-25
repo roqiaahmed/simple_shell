f18be054be24a96c51fba74f6e9d89723deaf3de
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-05-26 00:15:46 +0300

    update

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+int main(int ac, char **av)
+{
+	info_t info[] = {INFO_INIT};
+	int fd = 2;
+
+	asm("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r"(fd)
+		: "r"(fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				puts(av[0]);
+				puts(": 0: Can't open ");
+				puts(av[1]);
+				putchar('\n');
+				putchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
